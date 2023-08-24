39558e873332ee3b78cbd5665a24cbb0e2e2dddd
韩剑 <hanjian@umu.com>
2023-08-24 10:14:07 +0800

    完整的一个版本

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -42,6 +42,7 @@ return require('packer').startup(function(use)
     use 'folke/which-key.nvim'
     use 'akinsho/toggleterm.nvim'
     use 'lukas-reineke/indent-blankline.nvim'
+    use 'ahmedkhalf/project.nvim'
 
     use {
         'nvim-tree/nvim-tree.lua',
